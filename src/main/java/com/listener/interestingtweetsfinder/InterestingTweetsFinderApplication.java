ae30bffaee0630807c8a613dd6df5588e41a7efa
shubham <shubhamk0027@github.com>
2021-07-07 18:40:09 +0530

    Initial commit

---
+++ src/main/java/com/listener/interestingtweetsfinder/InterestingTweetsFinderApplication.java
@@ -0,0 +1,13 @@
+package com.listener.interestingtweetsfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestingTweetsFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterestingTweetsFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listener/interestingtweetsfinder/InterestingTweetsFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listener.interestingtweetsfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterestingTweetsFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
